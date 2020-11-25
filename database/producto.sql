bdf6b5dc87705166b8c5bd80ad492b60237810c1
Ivan Samudio <ivansamudio07@gmail.com>
2020-11-25 15:39:40 -0300

    terminado

--- database/producto.sql
+++ database/producto.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2020 a las 00:09:21
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.3
+-- Tiempo de generación: 25-11-2020 a las 19:39:15
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -39,9 +38,7 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id_producto`, `nombre`, `precio`) VALUES
-(0, 'harina', 110),
-(1, 'yerba', 100),
-(2, 'atun', 50);
+(1, 'yerba', 20);
 
 --
 -- Índices para tablas volcadas
@@ -52,6 +49,16 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `precio`) VALUES
 --
 ALTER TABLE `producto`
   ADD PRIMARY KEY (`id_producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
